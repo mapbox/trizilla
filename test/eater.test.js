0c8c6eed07b2b37bac960dc1ccf36df5ecc5bb09
ian29 <ian.villeda@gmail.com>
2015-02-09 15:53:57 -0500

    start eater tests

---
+++ test/eater.test.js
@@ -0,0 +1,10 @@
+var split = require('split')();
+var trizilla = require('../index');
+
+process.stdin.pipe(split)
+    .on('data', trizilla.eatStream)
+    .on('data', function(chunk){
+        try { var data = JSON.parse(chunk); }
+        catch(e) { throw e; }
+        console.log(data.key.length)
+    });
