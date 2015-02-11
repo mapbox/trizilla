71fad3fd9a1a7e7118a59b6633a54d7b3b8cac3c
ian29 <ian.villeda@gmail.com>
2015-02-10 19:48:58 -0500

    wrap things in funcitons more and pass minZ through

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 var util = require('util');
 var Transform = require('stream').Transform;
-var aggregator = require('./lib/aggregator');
+var aggregator = require('./lib/aggregator')();
 var inflator = require('./lib/inflator').inflate;
 //var tiler = require('./lib/tiler');
 
-var parentHolder = {}
+module.exports = function() {
+  var parentHolder = {}
 
-util.inherits(InflateStream, Transform);
-function InflateStream(minZ) { Transform.call(this, minZ) }
+  util.inherits(InflateStream, Transform);
+  function InflateStream(minZ) { Transform.call(this, minZ); this.minZ = minZ; }
+  // consume stream, inflate ∆s, and make parents
+  InflateStream.prototype._transform = function(chunk, enc, callback) {
 
-// consume stream, inflate ∆s, and make parents
-InflateStream.prototype._transform = function(chunk, enc, callback) {
+    try { var data = JSON.parse(chunk); }
+    catch(err) { callback(err); }
 
-  try { var data = JSON.parse(chunk); }
-  catch(err) { callback(err); }
-
-  this.push(inflator(data));
-  var minZ = minZ || (data.key.length-1)/2;
-  if ((data.key.length-1)/2 > minZ) {
-    var parent = ID.substring(0, ID.length-2);
-    // Does the parent object already exist? if so, aggregate its values; if not, initialize one
-    if (parentHolder[parent]) {
-      parentHolder[parent].aggregate(data);
-    } else {
-      parentHolder[parent] = new Aggregator();
-      parentHolder[parent].initialize(parent, data, function(err, child, pID) {
-        if (err) throw err;
-        this.push(inflator(child));
-        parentHolder[pID] = {}
-      });
+    console.log(aggregator);
+    this.push(inflator(data));
+    minZ = this.minZ || (data.key.length-1)/2;
+    if ((data.key.length-1)/2 > minZ) {
+      console.log('aggregating!');
+      var parent = data.key.substring(0, data.key.length-2);
+      // Does the parent object already exist? if so, aggregate its values; if not, initialize one
+      if (parentHolder[parent]) {
+        parentHolder[parent].aggregate(data);
+      } else {
+        parentHolder[parent] = new aggregator.Aggregator();
+        parentHolder[parent].initialize(parent, data, function(err, child, pID) {
+          if (err) throw err;
+          this.push(inflator(child));
+          parentHolder[pID] = {}
+        });
+      }
     }
+    callback();
   }
-  callback();
-}
-
-function tile() {
-    // some tiling magic here
-}
-
-module.exports = {
-  inflate: function() { return new InflateStream(); },
-  tile: function() { return new TileStream(); },
+  return {
+    inflate: function(value) { return new InflateStream(value); }
+    //tile: function() { return new TileStream(); }
+  }
 }
--- test/eater.test.js
+++ test/eater.test.js
@@ -1,8 +1,8 @@
 var split = require('split')();
-var trizilla = require('../index');
+var trizilla = require('../index')();
 
 process.stdin.pipe(split)
-    .pipe(trizilla.inflate(5))
+    .pipe(trizilla.inflate(3))
     .pipe(process.stdout);
     //.pipe(trizilla.tiler)
     // outputs tile-sized geojsons
