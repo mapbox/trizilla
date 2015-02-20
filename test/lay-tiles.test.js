b9fca88d19e1fce1c21f7c714930d817bce2772e
dnomadb <damon@mapbox.com>
2015-02-19 21:52:10 -0800

    fixing tile test

--- test/lay-tiles.test.js
+++ test/lay-tiles.test.js
@@ -4,13 +4,6 @@ var JSONStream = require('JSONStream');
 var split = require('split')();
 var tape = require('tape');
 
-function tileCallback(err, tileData) {
-  tape('should match the tiled feature', function{})
-  JSON.stringify(tileData)
-  var expected = fs.readFileSync('./test/fixtures/laytile-expected.txt').toString();
-  console.log(expected)
-}
-
 var stream = fs.createReadStream('./test/fixtures/trigeojsonstream.txt');
 
 var parser = JSONStream.parse();
@@ -18,6 +11,15 @@ var parser = JSONStream.parse();
 stream.pipe(parser);
 
 var tri_tiler = new trizilla_tiler.Tile()
+
+function tileCallback(err, tileData) {
+  tape('should match the tiled feature', function(t) {
+  t.ok(tri_tiler, 'is inflating');
+  var expected = JSON.parse(fs.readFileSync('./test/fixtures/laytile-expected.txt'));
+  t.equal(JSON.stringify(expected), JSON.stringify(tileData));
+  t.end();
+  })
+}
 
 var firstTime = true;
 
