b85e57ac069ca6a696ad455a00babec1c0260fc6
dnomadb <damon@mapbox.com>
2015-02-20 14:34:56 -0800

    tests working

--- test/lay-tiles.test.js
+++ test/lay-tiles.test.js
@@ -4,7 +4,7 @@ var JSONStream = require('JSONStream');
 var split = require('split')();
 var tape = require('tape');
 
-var stream = fs.createReadStream('./test/fixtures/trigeojsonstream.txt');
+var stream = fs.createReadStream('./test/fixtures/tri-geojson-stream');
 
 var parser = JSONStream.parse();
 
@@ -15,8 +15,8 @@ var tri_tiler = new trizilla_tiler.Tile()
 function tileCallback(err, tileData) {
   tape('should match the tiled feature', function(t) {
     t.ok(tri_tiler, 'is inflating');
-    var expected = JSON.parse(fs.readFileSync('./test/fixtures/laytile-expected.txt'));
-    t.equal(JSON.stringify(expected), JSON.stringify(tileData));
+    var expected = fs.readFileSync('./test/fixtures/laytile-expected').toString();
+    t.equal(JSON.stringify(tileData), expected);
     t.end();
   })
 }
@@ -26,10 +26,8 @@ var firstTime = true;
 parser.on('root', function (GeoJSON) {
   if (firstTime) {
     firstTime = false;
-    tri_tiler.initialize(GeoJSON, '0', 131072, tileCallback)
+    tri_tiler.initialize(GeoJSON, '021333303', 128, tileCallback)
   } else {
     tri_tiler.addFeature(GeoJSON)
   }
 });
-
-
