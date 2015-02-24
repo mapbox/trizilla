49c91465ca6a284cdb76058f65b4ecc76b0bebd0
dnomadb <damon@mapbox.com>
2015-02-23 17:24:35 -0800

    bin breakage

--- bin/trizilla-eat.js
+++
@@ -1,9 +0,0 @@
-var split = require('split')();
-var trizilla = require('../index')();
-
-process.stdin.pipe(split)
-    .pipe(trizilla.clean({}))
-    .pipe(trizilla.compress(1))
-    .pipe(trizilla.decompress({}))
-    .pipe(trizilla.inflate(6))
-    .pipe(process.stdout)
