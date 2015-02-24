63ae7165b3b678fc6bfe5d95e468603b95095d23
ian29 <ian.villeda@gmail.com>
2015-02-24 18:16:01 -0500

    remove trizilla.serialize, update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@ var split = require('split')();
 var trizilla = require('../index')();
 var fs = require('fs');
 var tape = require('tape');
+var UPDATE = process.env.UPDATE;
 
 tape('should load, parse, inflate, tile, and serialize a stream', function(t) {
   fs.createReadStream('./test/fixtures/fill-facets-output')
@@ -9,9 +10,9 @@ tape('should load, parse, inflate, tile, and serialize a stream', function(t) {
     .pipe(trizilla.clean({}))
     .pipe(trizilla.inflate(5))
     .pipe(trizilla.tile(3))
-    .pipe(trizilla.serialize())
     .on('data', function(data) {
       t.ok(trizilla, 'processed, checking')
+      if (UPDATE) fs.writeFileSync('./test/fixtures/eater-test-expected', data);
       var expected = fs.readFileSync('./test/fixtures/eater-test-expected').toString();
       var data = JSON.stringify(JSON.parse(data));
       t.equal(data, expected);
