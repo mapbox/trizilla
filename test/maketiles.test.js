f09f88b74af06edfa1ba588c86344e74e497d26e
dnomadb <damon@mapbox.com>
2015-03-02 12:11:44 -0800

    trying to parse vtile

--- test/maketiles.test.js
+++ test/maketiles.test.js
@@ -2,12 +2,21 @@ var trizilla_tilemaker = require('../lib/maketile');
 var fs = require('fs');
 var tape = require('tape');
 var Protobuf = require('pbf');
-var VectorTile = require('vector-tile').VectorTile;
+var VectorTile = require('vector-tile');
+var zlib = require('zlib');
 
 function tileTester(err, data) {
-  var pbf = new Protobuf(data);
-  var vtile = new VectorTile(pbf);
-  console.log(vtile.layers);
+  if (err) throw (err);
+  var tile = JSON.parse(data);
+
+  zlib.gunzip(data.buffer, function(err, data) {
+    var pbf = new Protobuf(data);
+    var vtile = new VectorTile.VectorTile(pbf);
+    console.log(vtile);
+  });
 }
 
-fs.readFile('./test/fixtures/laytile-expected', tileTester)
+fs.readFile('./test/fixtures/laytile-expected', function(err, data) {
+  if (err) throw (err);
+  trizilla_tilemaker.makeTile(JSON.parse(data), tileTester);
+})
