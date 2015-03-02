7cc303a1cd65be1ede8875b6f940302004414a6b
dnomadb <damon@mapbox.com>
2015-03-02 12:16:57 -0800

    changing tile names

--- test/maketiles.test.js
+++ test/maketiles.test.js
@@ -5,11 +5,12 @@ var Protobuf = require('pbf');
 var VectorTile = require('vector-tile');
 var zlib = require('zlib');
 
-function tileTester(err, data) {
+function tileTester(err, tile) {
   if (err) throw (err);
-  var tile = JSON.parse(data);
 
   zlib.gunzip(tile.buffer, function(err, data) {
+    if (err) throw (err);
+
     var pbf = new Protobuf(data);
     var vtile = new VectorTile.VectorTile(pbf);
     console.log(vtile);
